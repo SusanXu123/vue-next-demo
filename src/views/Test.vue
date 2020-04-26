2ec22548e206d5ca7d57b07aef94b3fa7778f57a
Xujuan <767196201@qq.com>
2020-04-26 15:19:34 +0800

    feat: 状态和事件绑定

---
+++ src/views/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="test">
+    <h1>test count: {{count}}</h1>
+    <button @click="add">add</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup () {
+    const count = ref(0)
+    const add = () => {
+      // 注意：需要用count.value++， 不是直接使用count++
+      count.value++
+    }
+    return {
+      count,
+      add
+    }
+  }
+}
+</script>
