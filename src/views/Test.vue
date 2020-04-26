06ce592c7a126d3397243169625ef39cc9de7465
Xujuan <767196201@qq.com>
2020-04-26 15:40:23 +0800

    feat: computed and watch

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="test">
     <h1>test count: {{count}}</h1>
+    <div>count * 2 = {{doubleCount}}</div>
     <button @click="add">add</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed, watch } from 'vue'
 export default {
   setup () {
     const count = ref(0)
@@ -14,9 +15,14 @@ export default {
       // 注意：需要用count.value++， 不是直接使用count++
       count.value++
     }
+    const doubleCount = computed(() => count.value * 2)
+    watch(() => count.value, (val, oldVal) => {
+      console.log('watch--', val, oldVal)
+    })
     return {
       count,
-      add
+      add,
+      doubleCount
     }
   }
 }
